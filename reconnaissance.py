a7ddf65f7a317fc230512aeba99b0fdf8ea7fb43
rawi142 <rawia.ben.amira1997@gmail.com>
2023-10-02 22:17:09 +0200

    calcul zcr

--- reconnaissance.py
+++ reconnaissance.py
@@ -1,13 +1,32 @@
-import numpy
-import matplotlib
-from matplotlib import pylab, mlab, pyplot
-np = numpy
-plt = pyplot
-from IPython.display import display
-from IPython.core.pylabtools import figsize, getfigs
-from pylab import *
-from numpy import *
+"""
+tp de reconnaissance simple
+"""
 
-b = open("data/0.raw", "r")
-b.decode('UTF-8')
-print(b.readlines())
+
+def ouverture_fichier_audio(fichier):
+    """
+    effectue l'ouverture d'un fichier audio et extrait son contenu
+    """
+    with open(fichier, "rb") as file:
+        audio_data = file.read()
+    audio_data = list(audio_data)
+    return audio_data
+
+
+def calcul_zcr(audio_data):
+    """
+    calcul le taux de passage par zero d un signal
+    en effet si une valeur est positive et que sa precedente est negative
+    ( et l inverse ) alors un passage par zero a eu lieu
+    """
+    zcr_audio = 0
+    for i in range(1, len(audio_data)):
+        if audio_data[i-1] * audio_data[i] < 0:
+            zcr_audio += 1
+    return zcr_audio
+
+
+if __name__ == "__main__":
+    audio_da = ouverture_fichier_audio("data/0.raw")
+    ZCR = calcul_zcr(audio_da)
+    print(ZCR)

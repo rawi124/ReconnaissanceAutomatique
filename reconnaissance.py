129a33a747117bcaf47a0ed58257813316b6beaf
rawi142 <rawia.ben.amira1997@gmail.com>
2023-10-03 12:25:33 +0200

    etiquettage

--- reconnaissance.py
+++ reconnaissance.py
@@ -4,15 +4,14 @@ dans un premier temps on va effectuer une parametrisation
 qui consiste a transformer un signal audio brut en une autre representation
 plus significative
 """
+import numpy as np
 
 
 def ouverture_fichier_audio(fichier):
     """
     effectue l'ouverture d'un fichier audio et extrait son contenu
     """
-    with open(fichier, "rb") as file:
-        audio_data = file.read()
-    audio_data = list(audio_data)
+    audio_data = np.fromfile(fichier)
     return audio_data
 
 
@@ -24,27 +23,42 @@ def calcul_zcr(audio_data):
     """
     zcr_audio = 0
     for i in range(1, len(audio_data)):
-        if audio_data[i-1] * audio_data[i] < 0:
+        # print(audio_data[i])
+        if np.float64(audio_data[i-1]) * np.float64(audio_data[i]) < 0:
             zcr_audio += 1
-    return zcr_audio
+    return zcr_audio/len(audio_data)
+
+
 def zcr_tous_fichiers(chemin):
     """
     calcule la zcr pour chaque fichier dans le repertoire en parametre
     """
-    for i in range(10):
-        fichier = chemin + "/" + str(i) + ".raw"
-        print(fichier)
+    zcr_s = []
+    lettres = "0123456789abcdefghijklmnopqrsuvwxyz"
+    for lettre in lettres:
+        fichier = chemin + "/" + lettre + ".raw"
         audio_data = ouverture_fichier_audio(fichier)
-        print(calcul_zcr(audio_data))
-    for i in ['a', 'b', 'c' ] :
-        fichier = chemin + "/" + i + ".raw"
-        print(fichier)
-        audio_data = ouverture_fichier_audio(fichier)
-        print(calcul_zcr(audio_data))
+        zcr_s.append(calcul_zcr(audio_data))
+    zcr_s_arrondies = [round(zcr, 1) for zcr in zcr_s]
+    return zcr_s_arrondies
+
+
+def etiquettage(zcr_s):
+    """
+    effectue ettiquetage en gardant que les taux 
+    de passage par zero qui ont plus de deux occurences
+    """
+    occurences = {}
+    for occ in zcr_s:
+        if occ in occurences:
+            occurences[occ] += 1
+        else:
+            occurences[occ] = 1
+    return {element: count for element, count in occurences.items() if count > 1}
 
 
 if __name__ == "__main__":
     audio_da = ouverture_fichier_audio("data/0.raw")
     ZCR = calcul_zcr(audio_da)
-    print(ZCR)
-    zcr_tous_fichiers("data")
+    ZCRS = zcr_tous_fichiers("data")
+    print(etiquettage(ZCRS))
